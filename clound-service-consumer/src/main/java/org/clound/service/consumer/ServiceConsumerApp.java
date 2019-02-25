59f5fa99854833755749d3345717193c7347cabb
machao0711 <18538526536@163.com>
2019-02-25 12:16:29 +0800

    测试

--- clound-service-consumer/src/main/java/org/clound/service/consumer/ServiceConsumerApp.java
+++ clound-service-consumer/src/main/java/org/clound/service/consumer/ServiceConsumerApp.java
@@ -1,9 +1,12 @@
 package org.clound.service.consumer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 服务消费方
@@ -16,9 +19,15 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 //它具有可插拔的注解支持包括Feign注解与JAX-RS注解SpringCloud对Feign进行了封装，使其支持SpringMVC标准注解和HttpMessageConverters。
 //Feign可以 Eureka和Ribbon组合使用以支持负载均衡。
 @EnableFeignClients
+@RestController
 public class ServiceConsumerApp {
-    public static void main( String[] args ){
-    	SpringApplication.run(ServiceConsumerApp.class, args);
-    	//new SpringApplicationBuilder(ServiceConsumerApp.class).web(true).run(args);
-    }
+	public static void main( String[] args ){
+		SpringApplication.run(ServiceConsumerApp.class, args);
+		//new SpringApplicationBuilder(ServiceConsumerApp.class).web(true).run(args);
+	}
+	@Value("${hello}") String hello;
+	@RequestMapping(value = "/hello")
+	public String hello(){
+		return hello;
+	}
 }
