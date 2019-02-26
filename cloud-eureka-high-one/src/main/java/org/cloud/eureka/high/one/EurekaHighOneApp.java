88af030e89e4b776bc8d47fbfd4dd458ddc8334d
machao0711 <18538526536@163.com>
2019-02-26 19:59:47 +0800

    idea

--- cloud-eureka-high-one/src/main/java/org/cloud/eureka/high/one/EurekaHighOneApp.java
+++ cloud-eureka-high-one/src/main/java/org/cloud/eureka/high/one/EurekaHighOneApp.java
@@ -1,8 +1,10 @@
 package org.cloud.eureka.high.one;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.util.Scanner;
 
 /**
  * 搭建注册者
@@ -12,6 +14,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaHighOneApp {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaHighOneApp.class, args);
+		//SpringApplication.run(EurekaHighOneApp.class, args);
+		Scanner scanner = new Scanner(System.in);
+		String profiles = scanner.nextLine();
+		new SpringApplicationBuilder(EurekaHighOneApp.class).profiles(profiles).run(args);
+
 	}
+
 }
