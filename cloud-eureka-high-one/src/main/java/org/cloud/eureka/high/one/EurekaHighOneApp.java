08bc928e9cbd357451913e8262b4219c026aa5e9
machao0711 <18538526536@163.com>
2019-02-26 09:20:59 +0800

    新的子模块

---
+++ cloud-eureka-high-one/src/main/java/org/cloud/eureka/high/one/EurekaHighOneApp.java
@@ -0,0 +1,17 @@
+package org.cloud.eureka.high.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 搭建注册者
+ */
+//该注解表明应用为eureka服务，有可以联合多个服务作为集群，对外提供服务注册以及发现功能
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaHighOneApp {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaHighOneApp.class, args);
+	}
+}
